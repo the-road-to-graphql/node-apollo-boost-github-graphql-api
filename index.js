38209fcc09ebf3e80f84c723d64fed4e8108989a
Robin Wieruch <wrobin@gmx.net>
2018-04-13 15:23:26 +0800

    add dotenv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const foo = { foo: 'foo' };
 const bar = { bar: 'bar' };
 const fooBar = { ...foo, ...bar };
 
 console.log(fooBar);
+console.log(process.env.GITHUB_PERSONAL_ACCESS_TOKEN);
