b2a22374958192a5a03dc821c853d7d82501f311
Robin Wieruch <wrobin@gmx.net>
2018-04-13 16:20:24 +0800

    iso fetch and first query

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+import 'cross-fetch/polyfill';
+import ApolloClient, { gql } from 'apollo-boost';
+
 require('dotenv').config();
 
-const foo = { foo: 'foo' };
-const bar = { bar: 'bar' };
-const fooBar = { ...foo, ...bar };
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
 
-console.log(fooBar);
-console.log(process.env.GITHUB_PERSONAL_ACCESS_TOKEN);
+const ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+
+client
+  .query({
+    query: ORGANIZATION,
+  })
+  .then(result => console.log(result));
