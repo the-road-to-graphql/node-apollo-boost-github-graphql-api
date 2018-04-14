0fb47682b09e35becd0212c9cb4b9e73c72f1fd7
Robin Wieruch <wrobin@gmx.net>
2018-04-14 10:53:49 +0800

    add query section

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import ApolloClient, { gql } from 'apollo-boost';
 
 require('dotenv').config();
 
+let state = {
+  organization: null,
+};
+
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   request: operation => {
@@ -14,20 +18,82 @@ const client = new ApolloClient({
   },
 });
 
-const ORGANIZATION = gql`
-  query($organization: String!) {
+// QUERY
+
+const REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!, $cursor: String) {
     organization(login: $organization) {
       name
       url
+      repositories(
+        first: 5
+        orderBy: { direction: DESC, field: STARGAZERS }
+        after: $cursor
+      ) {
+        edges {
+          node {
+            ...repository
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
     }
+  }
+
+  fragment repository on Repository {
+    name
+    url
   }
 `;
 
 client
   .query({
-    query: ORGANIZATION,
+    query: REPOSITORIES_OF_ORGANIZATION,
     variables: {
       organization: 'the-road-to-learn-react',
+      cursor: undefined,
     },
   })
-  .then(result => console.log(result));
+  // first page
+  .then(result => {
+    const { organization } = result.data;
+    const { endCursor } = organization.repositories.pageInfo;
+
+    state = {
+      organization,
+    };
+
+    console.log('first page', state.organization.repositories.edges.length);
+
+    return client.query({
+      query: REPOSITORIES_OF_ORGANIZATION,
+      variables: {
+        organization: 'the-road-to-learn-react',
+        cursor: endCursor,
+      },
+    });
+  })
+  // second page
+  .then(result => {
+    const { organization } = result.data;
+
+    state = {
+      organization: {
+        ...organization,
+        repositories: {
+          ...organization.repositories,
+          edges: [
+            ...state.organization.repositories.edges,
+            ...organization.repositories.edges,
+          ],
+        },
+      },
+    };
+
+    console.log('second page', state.organization.repositories.edges.length);
+  });
+
+// MUTATION
