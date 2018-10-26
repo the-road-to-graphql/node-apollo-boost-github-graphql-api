fd067ec045861e9832cc0b202b25f8d8efd651c9
Robin Wieruch <wrobin@gmx.net>
2018-10-26 11:14:05 +0800

    01-Starting with Apollo Boost on the Command Line

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import 'cross-fetch/polyfill';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
